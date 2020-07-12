732e04e27e8a82e7130bee14f9f4737f749bb8b6
Braj Kishor Gupta <brajgupta1741012351@gmail.com>
2020-07-12 13:48:20 +0530

    Updates

---
+++ precache-manifest.941459875c9ab6b1ec19892e5be7d0f0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bde8f5c225b607759224effc65d41af",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "c4ea3f038f82a7046d41",
+    "url": "/Portfolio/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "bf219515f82a8f9e4b62",
+    "url": "/Portfolio/static/css/main.3c5c2e7d.chunk.css"
+  },
+  {
+    "revision": "c4ea3f038f82a7046d41",
+    "url": "/Portfolio/static/js/2.f194ae1a.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/Portfolio/static/js/2.f194ae1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf219515f82a8f9e4b62",
+    "url": "/Portfolio/static/js/main.aa340ad8.chunk.js"
+  },
+  {
+    "revision": "796235efddf27470bbfe",
+    "url": "/Portfolio/static/js/runtime-main.25f07560.js"
+  },
+  {
+    "revision": "7cc4e33d7e790b93a44dc7ebac9bc1a7",
+    "url": "/Portfolio/static/media/sahil.7cc4e33d.jpg"
+  }
+]);
